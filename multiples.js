8a592b262f146e5912c2cb45a67cb1766694a144
Guillermo Romero <garomero1994@yahoo.com>
2015-01-14 19:49:08 -1000

    commented out previous answer and solved same problem without using for loop

--- multiples.js
+++ multiples.js
@@ -6,10 +6,30 @@
 exports.sumOfAMultiple = function(n){
   var sum = 0;
   // do your work here
-  for (i = 1; i< n; i++) {
-    if ((i % 3 === 0) || (i % 5 === 0)) {
-      sum = sum + i;
-    }
-  }
+  // for (i = 1; i< n; i++) {
+  //   if ((i % 3 === 0) || (i % 5 === 0)) {
+  //     sum = sum + i;
+  //   }
+  // }
+
+  var sum3, sum5, sum15;
+  var count;
+  // 1. find sum of all multiples of 3 less than n
+  // 2. find sum of all multiples of 5 less than n
+  // 3. find sum of all multiples of 15 less than n
+  // 4. result 1 + result 2 - result 3 = our intended result
+  count = Math.floor(( n - 1 ) / 3);
+  sum3 = 3 * count * (count + 1) / 2;
+
+  count = Math.floor(( n - 1 ) / 5);
+  sum5 = 5 * count * (count + 1) / 2;
+
+  count = Math.floor(( n - 1 ) / 15);
+  sum15 = 15 * count * (count + 1) / 2;
+
+  sum = sum3 + sum5 - sum15;
+
+
   return sum;
 };
+
